4d265349e0fdb74e458d10490f16a4eba9db36e0
Avik Sinha <aviksinha941@gmail.com>
2025-02-23 01:15:59 +0530

    dismissal thing done

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -39,6 +39,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +57,12 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           Text("The Chart"),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
+          ),
         ],
       ),
     );
