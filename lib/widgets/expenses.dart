c942dd11942ba2c96f216454e6f833fb15d1bb45
Avik Sinha <aviksinha941@gmail.com>
2025-02-22 12:01:44 +0530

    Added new files

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,49 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter course",
+      category: Category.work,
+      amount: 19.99,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: "movie",
+      category: Category.leisure,
+      amount: 2009,
+      date: DateTime.now(),
+    ),
+  ];
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter expense tracker"),
+        actions: [
+          IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add)),
+        ],
+      ),
+      body: Column(
+        children: [
+          Text("The Chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
